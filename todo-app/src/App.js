21159f44f7756df68028e9e3749836a04ab81001
seenew22 <seenew22@naver.com>
2021-09-28 19:16:14 +0900

    [feat] TodoListItem / TodoList 작성 완료

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
-
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 }
